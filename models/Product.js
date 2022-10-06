10a647ce58ae65d391220ec4d2c98a366ab62fa1
mlcundayag <mlcundayag@gmail.com>
2022-10-05 23:00:58 -1000

    set up tag model

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,7 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define columns: DONE
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
