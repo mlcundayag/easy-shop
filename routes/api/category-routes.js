c7842f0166dbb85f72ee2008a5f88f6ff059a642
mlcundayag <mlcundayag@gmail.com>
2022-10-06 21:24:51 -1000

    update package and cteated first route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,45 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [
+        {
+          model: Product,
+          attributes: [
+            'id',
+            'product_name',
+            'price',
+            'stock',
+            'catgeory_id'
+          ]
+        }
+      ]
+    })
+    res.json(categoryData);
+  } catch (error) {
+    res.status(500).json(error)
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
+// router.get('/:id', (req, res) => {
+//   // find one category by its `id` value
+//   // be sure to include its associated Products
+// });
 
-router.post('/', (req, res) => {
-  // create a new category
-});
+// router.post('/', (req, res) => {
+//   // create a new category
+// });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
+// router.put('/:id', (req, res) => {
+//   // update a category by its `id` value
+// });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
+// router.delete('/:id', (req, res) => {
+//   // delete a category by its `id` value
+// });
 
 module.exports = router;
