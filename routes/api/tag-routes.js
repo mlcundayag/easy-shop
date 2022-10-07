b9002f59b06df3cd3db89bee704c7685a32acdb1
mlcundayag <mlcundayag@gmail.com>
2022-10-06 22:54:28 -1000

    added tag routes: get

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,72 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [
+        {
+          model: Product,
+          attributes: [
+            'id',
+            'product_name',
+            'price',
+            'stock',
+            'category_id'
+          ]
+        }
+      ]
+    });
+    res.json(tagData);
+  }
+  catch (error) {
+    res.status(507).json(error)
+   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagID = await Tag.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+        {model: Product,
+          attributes: [
+            'id',
+            'product_name',
+            'price',
+            'stock',
+            'category_id'
+          ]
+        }
+      ]
+    });
+    if(tagID) {
+      res.json(tagID);
+    } else {
+      res.status(404).json({ error: "Tag not found, try a different ID"});
+    }
+  }
+  catch (error) {
+    res.status(508).json(error)
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
-});
+// router.post('/', (req, res) => {
+//   // create a new tag
+// });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
+// router.put('/:id', (req, res) => {
+//   // update a tag's name by its `id` value
+// });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
+// router.delete('/:id', (req, res) => {
+//   // delete on tag by its `id` value
+// });
 
 module.exports = router;
